02f7488616d2b5734323c41a4e2f0d1af5daed45
Juan Camejo <jcamejob@gmail.com>
2019-11-19 17:12:00 +0100

    Added Dashboard components

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-
-
+import { Router, Route, Switch } from 'react-router-dom';
+import Sidebar from './components/sidebar';
+import Main from './components/main';
+import Products from './components/products';
 import theme from './style.module.scss';
 
 const Dashboard = ({ match }) => {
-
     return(
         <div className={theme.container}>
             <div className={theme.header}>HEADER</div>
             <div className={theme['sub-container']}>
                 <div className={theme.sidebar}>
-                    
+                    <Sidebar match={match} />
                 </div>
                 <div className={theme.content}>
-
+                    <Switch>
+                        <Route exact path="/dashboard" component={Main}/>
+                        <Route exact path="/dashboard/products" component={Products}/>
+                    </Switch>
                     
                  </div>
             </div>
