dd6081559147b5335951945e56f4fdbd46e86261
Juan Camejo <jcamejob@gmail.com>
2019-11-21 00:45:18 +0100

    Added product form error message, field validator and fixed design

--- src/scenes/dashboard/components/products/components/product-form/ProductForm.jsx
+++ src/scenes/dashboard/components/products/components/product-form/ProductForm.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import theme from "./theme.module.scss";
 
-const ProductForm = ({ handleCreateProduct }) => {
+const ProductForm = ({ product, onSubmit }) => {
   const [formValues, setFormValues] = useState({
-    name: "",
-    price: "",
-    category: ""
+    name: product ? product.name : "",
+    price: product ? product.price : "",
+    category: product ? product.category : "",
   });
-
+  const [ errorMessage, setErrorMessage ] = useState('');
+  
   const changeName = event => {
     const newName = event.target.value;
     setFormValues({ ...formValues, name: newName });
@@ -15,6 +16,11 @@ const ProductForm = ({ handleCreateProduct }) => {
 
   const changePrice = event => {
     const newPrice = event.target.value;
+    const regex = new RegExp("^[0-9]*$");
+    
+    if(!regex.exec(newPrice)) {
+      return;
+    }
     setFormValues({ ...formValues, price: newPrice });
   };
 
@@ -23,49 +29,75 @@ const ProductForm = ({ handleCreateProduct }) => {
     setFormValues({ ...formValues, category: newCategory });
   };
 
+  const checkFormError = () => {
+    const {
+      name,
+      price,
+      category,
+    } = formValues;
+
+    if(!name || !price || !category) {
+      setErrorMessage("Debes llenar todos los campos");
+      return false;  
+    }
+
+    return true;
+  }
+
   const handleSubmit = event => {
       event.preventDefault();
-      handleCreateProduct(formValues);
+      const noError = checkFormError();
+
+      if(noError) {
+        onSubmit(formValues);
+      }
   }
 
   return (
     <div className={theme.container}>
       <form onSubmit={handleSubmit}>
-        <h1 className={theme["form-title"]}>Nuevo producto</h1>
-        <label for="name">Nombre producto: </label>
+        <h1 className={`${theme["form-title"]} ${product ? theme.edit : theme.create}`}>
+          { product ? "Editar producto" : "Nuevo producto"}
+        </h1>
+        <label htmlFor="name">Nombre producto: </label>
         <input
           type="text"
           name="name"
           id="name"
+          maxLength="20"
           value={formValues.name}
           onChange={changeName}
-          placeholder="Nombre del producto"
         />
-        <label for="price">Precio: </label>
+        <label htmlFor="price">Precio: </label>
         <input
           type="text"
           name="price"
           id="price"
+          maxLength="8"
           value={formValues.price}
           onChange={changePrice}
-          placeholder="Precio del producto"
         />
-        <label for="category">Categoria: </label>
+        <label htmlFor="category">Categoria: </label>
         <select
           name="category"
           id="category"
           value={formValues.category}
           onChange={changeCategory}
         >
-          <option>Ropa</option>
-          <option>Joyeria</option>
-          <option>Electrodomesticos</option>
+          <option defaultChecked>Electrodomesticos</option>
+          <option>Patinetes</option>
+          <option>Cocina</option>
+          <option>Aspiración</option>
+          <option>Cuidado Personal</option>
         </select>
         <div className={theme.rule}></div>
         <div className={theme['form-footer']}>
-          <button type="submit" className={theme.submit}>
+          <button type="submit" className={`${theme.submit} ${product ? theme.edit : theme.create}`}>
             Agregar producto
           </button>
+          { errorMessage &&
+            <p className={theme['error-message']}>{errorMessage}</p>
+          }
         </div>
       </form>
     </div>
