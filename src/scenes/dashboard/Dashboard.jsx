c32c23def8564eebf0996c672fb1d815fc487d9a
Juan Camejo <jcamejob@gmail.com>
2019-11-21 00:40:13 +0100

    Dashboard UI changes

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import Sidebar from './components/sidebar';
-import Main from './components/main';
-import Products from './components/products';
-import theme from './style.module.scss';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import Sidebar from "./components/sidebar";
+import Main from "./components/main";
+import Products from "./components/products";
+import theme from "./style.module.scss";
 
 const Dashboard = ({ match }) => {
-    return(
-        <div className={theme.container}>
-            <div className={theme.header}>HEADER</div>
-            <div className={theme['sub-container']}>
-                <div className={theme.sidebar}>
-                    <Sidebar match={match} />
-                </div>
-                <div className={theme.content}>
-                    <Switch>
-                        <Route exact path="/dashboard" component={Main}/>
-                        <Route exact path="/dashboard/products" component={Products}/>
-                    </Switch>
-                    
-                 </div>
-            </div>
+  return (
+    <div className={theme.container}>
+      <div className={theme.header}>
+				<h1>Bienvenido</h1>
+				<div>
+				<i className="material-icons">
+					exit_to_app
+				</i>
+				</div>
+			</div>
+      <div className={theme["sub-container"]}>
+        <div className={theme.sidebar}>
+          <Sidebar match={match} />
         </div>
-    )
+        <div className={theme.content}>
+          <Switch>
+            <Route exact path="/dashboard" component={Main} />
+            <Route exact path="/dashboard/products" component={Products} />
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
