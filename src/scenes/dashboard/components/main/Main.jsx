fb4c559b2038488383f3de26cfa431b632f64684
Juan Camejo <jcamejob@gmail.com>
2019-11-21 03:07:08 +0100

    Added welcome message

--- src/scenes/dashboard/components/main/Main.jsx
+++ src/scenes/dashboard/components/main/Main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import theme from './theme.module.scss';
 
 const Main = () => {
-    return(
-        <div>
-            <h1>BIENVENIDO</h1>
-        </div>
-    )
+  return (
+    <div className={theme.container}>
+      <h1>BIENVENIDO AL PANEL E-COMMERCE DE PRUEBA</h1>
+			<h1>PARA</h1>
+      <img src="https://2019.es.pycon.org/media/sponsor_files/Cecotec_logotipo_1.png" alt="logo cecotec"></img>
+    </div>
+  );
 };
 
 export default Main;
